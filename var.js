e78bc1469601b3b8b6a580133ac633364c8100ae
godgodwinter <kakadz.pro@gmail.com>
2021-11-09 09:18:30 +0700

    Initial commit

---
+++ var.js
@@ -0,0 +1,42 @@
+// ARRAY
+const namaSiswa=["Paijo","Joko","Tono"];
+
+namaSiswa.forEach(function (item,index,Array){
+        console.log(item,index);
+});
+
+namaSiswa.push("devi");
+// // menghaspus di akhir
+// namaSiswa.pop();
+// //menghapus di awal
+// namaSiswa.shift();
+
+console.log(namaSiswa);
+
+// index array ke berapa
+console.log(namaSiswa.indexOf("Joko"));
+
+
+// tipe data dasar
+// let iceCream=5;
+// let iceCream='Paijo';
+// let name='Paijo';
+// let iceCream=5%2;
+// let iceCream=2;
+
+// let iceCream=2;
+
+// let hasil=iceCream!=2?'Bukan Dua Icekrem':'Dua Eskrem';
+// console.log(hasil);
+
+
+// console.log(`${name} punya ${iceCream} Eskrim`)
+
+
+// if(iceCream >1) console.log(iceCream+'lebih dari 1')
+// else console.log(iceCream+ 'kurang dari = 1')
+
+
+
+// console.log(iceCream);
+// console.log(typeof iceCream);
